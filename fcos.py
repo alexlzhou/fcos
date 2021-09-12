9ee417e40ee20c59a97a3fb8bf76fb3e600ccea1
Alex Zhou <alex.lawsonzhou@hotmail.com>
2021-09-11 20:36:04 -0400

    fcos - bottleneck initial

--- fcos.py
+++ fcos.py
@@ -11,8 +11,45 @@ def conv3x3(in_planes, out_planes, stride=1):
 
 
 class Bottleneck(nn.Module):
-    def __init__(self):
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1, downsample=None):
         super(Bottleneck, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * 4)
+
+        self.relu = nn.ReLU(inplace=True)
+
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
 
 class FCOS(nn.Module):
     def __init__(self):
