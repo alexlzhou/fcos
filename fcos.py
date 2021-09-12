3edab042665693ea1d9677d0df12330330d41d26
Alex Zhou <alex.lawsonzhou@hotmail.com>
2021-09-11 21:32:53 -0400

    resnet, again

--- fcos.py
+++ fcos.py
@@ -51,6 +51,57 @@ class Bottleneck(nn.Module):
         out = self.relu(out)
 
 
+class ResNet(nn.Module):
+    def __init__(self, block, layers, num_classes=1000, include_top=False):
+        super(ResNet, self).__init__()
+
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+
+        self.avgpool = nn.AvgPool2d(7, stride=1)
+
+        if include_top:
+            self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        self.include_top = include_top
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+
+        if stride != 1 or self.in_planes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_planes, planes * block.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.in_planes, planes, stride, downsample))
+
+        self.in_planes = planes * block.expansion
+
+        for i in range(1, blocks):
+            layers.append(block(self.in_planes, planes))
+
+        return nn.Sequential(*layers)
+
+
 class FCOS(nn.Module):
     def __init__(self):
         super(FCOS, self).__init__()
